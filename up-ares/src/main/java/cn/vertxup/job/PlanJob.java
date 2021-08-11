20a5618b12dc34c3d781899f9eb51e1d820d0418
Lang <silentbalanceyh@126.com>
2021-08-11 20:06:56 +0800

    最新

--- up-ares/src/main/java/cn/vertxup/job/PlanJob.java
+++ up-ares/src/main/java/cn/vertxup/job/PlanJob.java
@@ -3,12 +3,9 @@ package cn.vertxup.job;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.up.annotations.Contract;
-import io.vertx.up.annotations.Job;
 import io.vertx.up.annotations.On;
 import io.vertx.up.commune.Envelop;
-import io.vertx.up.eon.em.JobType;
 
-@Job(value = JobType.PLAN, duration = 300, name = "Plan")
 public class PlanJob {
     @Contract
     private transient Vertx vertx;
